f4393ec9b1c234669ca04f6811236553f2072d4f
EduardBBlea <71567908+EduardBBlea@users.noreply.github.com>
2021-01-18 11:32:37 +0200

    initial commit

---
+++ app.js
@@ -0,0 +1,34 @@
+const optionsWrapper = document.querySelector(".img-container");
+const playerValueEl = document.querySelector(".player-value");
+const cpuValueEl = document.querySelector(".cpu-value");
+const outcomeEl = document.querySelector(".outcome");
+
+let playerOption = "";
+let cpuOption = "";
+
+
+  const gameOutcome = (cpu,player)=> {
+    if(cpu === player){outcomeEl.innerText = "It's a tie!!!"}
+    else if(cpu === "Paper" && player === "Rock" ||
+        cpu === "Scissors" && player === "Paper"||
+        cpu === "Rock" && player === "Scissors") { outcomeEl.innerText = "Cpu Wins!!!"}
+        else { outcomeEl.innerText = "Player wins!!!"}
+ };
+
+ const cpuPick = () => {
+   let rand = Math.random();
+   if(rand < 0.33){cpuOption = "Rock"}
+   else if (rand >0.33 && rand < 0.66) {cpuOption="Paper"}
+   else {cpuOption = "Scissors"};
+  cpuValueEl.innerText = cpuOption;
+ }
+
+optionsWrapper.addEventListener("click",((e)=>{
+  if(!e.target.classList.contains("far")) return;
+  playerOption = e.target.classList[0];
+  playerValueEl.innerText = playerOption;
+  cpuPick();
+  gameOutcome(cpuOption,playerOption);
+ }
+));
+
